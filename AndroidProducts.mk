ffb985a23a8197841a7fb1ca296675660c872847
Dhruvesh16 <dhruveshgit@gmail.com>
2022-01-01 19:30:53 +0530

    RMX1921: Changes for PixelExtended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_RMX1921.mk
+    $(LOCAL_DIR)/aosp_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_RMX1921-userdebug \
-    derp_RMX1921-eng
+    aosp_RMX1921-userdebug \
+    aosp_RMX1921-eng
