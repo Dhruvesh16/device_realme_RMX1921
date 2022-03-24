7d28488b3c8ab98363a0adaa3eb5d829ae93f01e
dhruvesh <dhruveshgit@gmail.com>
2022-03-24 23:56:26 +0530

    RMX1921:Adding Blaze Official Stuff

--- blaze_RMX1921.mk
+++ blaze_RMX1921.mk
@@ -4,11 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Spark stuff
+# Inherit some common Blaze stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 EXTRA_UDFPS_ANIMATIONS := true
 $(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
+
+#Blaze Official Stuff
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := Dhruvesh
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
