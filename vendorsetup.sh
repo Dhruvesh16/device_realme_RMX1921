7015312daf17bd2f8c870d58ef8d736ae4fbbc19
Dhruvesh16 <dhruvesh3466@protonmail.com>
2022-08-13 12:18:13 +0530

    RMX1921: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Dhruvesh16/android_kernel_realme_RMX1921.git -b 11 kernel/realme/RMX1921 
+git clone https://gitlab.com/Dhruvesh16/vendor_realme_RMX1921.git -b 12 vendor/realme/RMX1921 
