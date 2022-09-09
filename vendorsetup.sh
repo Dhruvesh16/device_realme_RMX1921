aecf828338c47f2319c961272e3f5cb5f07da2cf
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-09-09 13:36:22 +0530

    RMX1921: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Dhruvesh16/android_kernel_realme_RMX1921 -b 11 kernel/realme/RMX1921
+git clone https://gitlab.com/Dhruvesh16/vendor_realme_RMX1921 -b 12 vendor/realme/RMX1921
+git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton 
