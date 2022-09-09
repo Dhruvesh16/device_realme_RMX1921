2bfb5cf8dbeebf20e88309741764fb8cad193f9d
dhruvesh16 <dhruvesh3466@protonmail.com>
2022-09-09 11:21:29 +0530

    RMX1921: Nuke neutron clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,3 @@ git clone https://github.com/Dhruvesh16/android_kernel_realme_RMX1921.git -b 11 
 
 # Vendor Tree
 git clone https://gitlab.com/Dhruvesh16/vendor_realme_RMX1921.git -b 12 vendor/realme/RMX1921
-
-# Clang
-git clone --depth=1 https://github.com/Neutron-Clang/neutron-toolchain -b Neutron-14 prebuilts/clang/host/linux-x86/clang-neutron
